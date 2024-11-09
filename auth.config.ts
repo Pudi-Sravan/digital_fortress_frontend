ec1615cfacd09a2b6c9e85984232c18cda4bbb1c
git-leo-here <ankit2003.ar15@gmail.com>
2024-11-09 07:17:50 +0530

    removed next-auth-http-adapter

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,5 @@
 import GoogleProvider from "next-auth/providers/google"
 import type { NextAuthConfig } from "next-auth"
-import myHttpAdapter from "./adapter"
  
 export default { 
     providers: [GoogleProvider],
